26e0151d8eb1eb449b02e9efaa7e58c3c22dc6a2
abrorb3k <abrorb3k0219@gmail.com>
2024-11-23 15:51:18 +0500

    vazifa bajarildi

---
+++ main.js
@@ -0,0 +1,357 @@
+
+//1-masala
+// let son1 = +prompt("sonni kiriting");
+// if(son1>0){
+//     console.log(1)
+// }else{
+//     console.log("Berilgan son musbat emas")
+// }
+
+
+//2-masala
+// let son2 = +prompt("sonni kiriting");
+// if(son2<0){
+//     console.log(-1)
+// }else{
+//     console.log(0)
+// }
+
+
+//3-masala
+// let son3 = +prompt("sonni kiriting");
+// if(son3=0){
+//     console.log(1)
+// }else{
+//     console.log(0)
+// }
+
+
+//4-masala
+// let son4 = +prompt("sonni kiriting");
+// if(son4>100){
+//     console.log(1)
+// }else{
+//     console.log("Berilgan son 100 dan kichik")
+// }
+
+
+//5-masala
+// let son5 = +prompt("sonni kiriting");
+// if ( son5<0 && son5 < 10) {
+//   console.log(-1);
+// } else {
+//   console.log("berilgan son xato");
+// }
+
+
+//6-masala
+// let son6=+prompt("sonni kiriting")
+// if(son6>0){
+//     console.log(son6**2)
+// }else{
+//     console.log(0)
+// }
+
+
+//7-masala
+// let son7 = +prompt("sonni kiriting");
+// if(son7<0){
+//     son7=-son7
+// }
+// console.log(son7);
+
+
+//9-masala
+// let son8=+prompt("sonni kiriting")
+// if(son8==0){
+//     console.log(10)
+// }else{
+//     console.log(son8)
+// }
+
+
+//10-masala
+// let son10=+prompt("sonni kiriting")
+// if(son10<0){
+//     console.log(-10)
+// }else{
+//     console.log(10)
+// }
+
+
+//11-masala
+// let son11=+prompt("sonni kiriting")
+// if(son11%2==0){
+//     console.log(2)
+// }else{
+//     console.log(1)
+// }
+
+
+//12-masala
+// let son12=+prompt("sonni kiriting")
+// if(son12%2==1){
+//     console.log(3)
+// }else{
+//     console.log(4)
+// }
+
+
+//13-masala
+// let son13=+prompt("sonni kiriting")
+// if(son13>10 && son13%2==0){
+//     console.log(1)
+// }else{
+//     console.log("Berilgan son noto'g'ri")
+// }
+
+
+//14-masala
+// let son14=+prompt("sonni kiriting")
+// if (son14 < 0 && son14 % 2!== 1) {
+//   console.log(-1);
+// } else {
+//   console.log("Berilgan son noto'g'ri");
+// }
+
+
+//15-masala
+// let son15=+prompt("sonni kiriting")
+// if(son15%2==0 && son15%5==0){
+//     console.log(100)
+// }else{
+//     console.log("Berilgan son xato")
+// }
+
+
+//16-masala
+// let son16=+prompt("sonni kiriting")
+// if(son16%2==1){
+//     console.log(son16**2)
+// }else{
+//     console.log("Berilgan son noto'g'ri");
+// }
+
+
+//17-masala
+// let son17=+prompt("sonni kiriting")
+// if(son17%2==0 && son17>0){
+//     console.log(son17**3)
+// }else{
+//     console.log("Berilgan son noto'g'ri");
+// }
+
+
+//18-masala
+// let son18=+prompt("sonni kiriting")
+// if(son18>20 && son18%2==0){
+//     console.log(50)
+// }else{
+//     console.log(-50);
+// }
+
+
+//19-masala
+// let son19=+prompt("sonni kiriting")
+// if(son19%7==0 && son19%2==0){
+//     console.log(7)
+// }else{
+//     console.log("Berilgan son xato")
+// }
+
+
+//20-masala
+// let son20=+prompt("sonni kiriting")
+// if (son20 % 2 == 0 && son20 < 0) {
+//   console.log(-20);
+// } else {
+//   console.log("Berilgan son xato");
+// }
+
+
+//21-masala
+// let son21=+prompt("1-sonni kiriting")
+// let son211=+prompt("2-sonni kiriting")
+// if (son21 == son211) {
+//   console.log(1);
+// } else {
+//   console.log(0);
+// }
+
+
+//22-masala
+// let son22=+prompt("1-sonni kiriting")
+// let son222=+prompt("2-sonni kiriting")
+// if(son22>son222){
+//   console.log(2)
+// } else{
+//     console.log(3)
+// }
+
+
+//23-masala
+// let son23 = +prompt("1-sonni kiriting")
+// let son233 = +prompt("2-sonni kiriting")
+// if(son23<son233 && son23>0){
+//     console.log(10)
+// }else{
+//     console.log("Berilgan son xato")
+// }    
+
+
+//24-masala
+// let son24 = +prompt("1-sonni kiriting")
+// let son244 = +prompt("2-sonni kiriting")
+// if(son24>son244 && son24%2==1){
+//     console.log(5)
+// }else{
+//     console.log("Berilgan son xato")
+// }
+
+
+//25-masala
+// let son25 = +prompt("1-sonni kiriting")
+// let son255 = +prompt("2-sonni kiriting")
+// if(son25>0 && son255>0){
+//     console.log(son25+son255)
+// }else{
+//      console.log("Berilgan son xato");
+// }
+
+
+//26-masala
+//let son26 = +prompt("1-sonni kiriting")
+// let son266 =+prompt("2-sonni kiriting")
+// if(son26<son266 && son266%2==0){
+//     console.log(-1)
+// }else{
+//     console.log("Berilgan son xato");
+// }
+
+
+//27-masala
+// let son27 = +prompt("1-sonni kiriting")
+// let son277 = +prompt("2-sonni kiriting")
+// if(son27==son277){
+//     console.log(100)
+// }else{
+//     console.log(-100);
+// }
+
+
+//28-masala
+// let son28 = +prompt("1-sonni kiriting")
+// let son288 = +prompt("2-sonni kiriting")
+// if (son28 % 10 == 0 && son288 % 5 == 0) {
+//   console.log(50);
+// } else {
+//   console.log("Berilgan son xato");
+// }
+
+
+// 29-masala
+// let son29 = +prompt("1-sonni kiriting")
+// let son299 = +prompt("2-sonni kiriting")
+// if(son29/2==son299){
+//     console.log(2)
+// }else{
+//      console.log("Berilgan son xato")
+// }
+
+
+//30-masala
+// let son30 = +prompt("1-sonni kiriting")
+// let son300 = +prompt("2-sonni kiriting")
+// if(son30<son300 && son300%2==1){
+//     console.log(-10)
+// }
+
+
+//31-masala
+// let son31=+prompt("sonni kiriting")
+// if(son31>0 && son31%3==0){
+//     console.log(3)
+// }else{
+//     console.log(0)
+// }
+
+
+//32-masala
+// let son32=+prompt("sonni kiriting")
+// if(son32<0 && son32%2==0){
+//     console.log(-2)
+// }else{
+//     console.log("Berilgan son xato")
+// }
+
+
+//33-masala
+// let son33=+prompt("sonni kiriting")
+// if(son33>0 ){
+//     console.log(son33%5)
+// }else{
+//     console.log("Berilgan son xato")
+// }
+
+
+//34-masala
+// let son34=+prompt("sonni kiriting")
+// if(son34>0 && son34<10){
+//     console.log(son34**2)
+// }else{
+//     console.log("Berilgan son xato")
+// }
+
+
+//35-masala
+// let son36=+prompt("sonni kiriting")
+// if(son36%2==0 && son36%4==0){
+//     console.log(1)
+// }else{
+//     console.log("Berilgan son xato")
+// }
+
+
+//36-masala
+// let son36=+prompt("sonni kiriting")
+// if(son36>15 && son36%2==1){
+//     console.log(15)
+// }else{
+//     console.log("Berilgan son xato")
+// }
+
+
+//37-masala
+// let son37=+prompt("sonni kiriting")
+// if(son37%2==0 && son37%3==0){
+//     console.log(6)
+// }else{
+//     console.log(1)
+// }
+
+
+//38-masala
+// let son38=+prompt("sonni kiriting")
+// if(son38%7==0 && son38>0){
+//     console.log(14)
+// }else{
+//     console.log("Berilgan son xato")
+// }
+
+
+//39-masala
+// let son39=+prompt("sonni kiriting")
+// if(son39>=0){
+//     console.log(0)
+// }else{
+//     console.log(-5);
+// }
+
+
+//40-masala
+// let son40 = +prompt("sonni kiriting");
+// if(son40<0 && son40%10!==0){
+//     console.log(-3)
+// }else{
+//     console.log("Berilgan son xato")
+// }
